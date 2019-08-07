c70d49cff245f3d37a6bc4a844ad7aeb09ac0ea1
Fabiano <fabinppk@hotmail.com>
2019-08-07 14:13:20 -0300

    feat: like/dislike/getdevs routes

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,17 +1,36 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
+const axios = require("axios");
+const Dev = require("../models/Dev");
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+    console.log(user);
+
+    const loggedUser = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedUser.likes } },
+        { _id: { $nin: loggedUser.dislikes } }
+      ]
+    });
+
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
 
     const userExist = await Dev.findOne({ user: username });
 
-    if(userExist) {
+    if (userExist) {
       return res.json(userExist);
     }
 
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
 
     const { name, bio, avatar_url: avatar } = response.data;
 
@@ -20,8 +39,8 @@ module.exports = {
       user: username,
       bio,
       avatar
-    })
+    });
 
     return res.json(dev);
   }
-}
+};
--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
 
 const server = express();
 
 mongoose.connect(
-  'mongodb+srv://semana:semana@cluster0-pggb2.mongodb.net/omnistackTindev?retryWrites=true&w=majority',
+  "mongodb+srv://semana:semana@cluster0-pggb2.mongodb.net/omnistackTindev?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
+
+server.use(cors());
 
 server.use(express.json());
 
 server.use(routes);
 
-server.listen(3333)
+server.listen(3333);
